79662d6d5b822e5ed18961022c1de7509087c271
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-12-16 05:02:00 -0500

    Update pusher.ts

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -5,11 +5,11 @@ const config = {
   appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID as string,
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY as string,
   secret: process.env.NEXT_PUBLIC_PUSHER_APP_SECRET as string,
-  cluster: "eu",
+  cluster: "us2",
   useTLS: Boolean(process.env.NEXT_PUBLIC_PUSHER_APP_TLS!),
 };
 export const pusherServer = new PusherServer({ ...config });
 
 export const pusherClient = new PusherClient(config.key, {
-  cluster: "eu",
+  cluster: "us2",
 });
