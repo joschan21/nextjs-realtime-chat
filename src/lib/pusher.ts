80a3f3d0a4945bbdeddd53a31722cdea2978ef85
Yuvaraj Nagi <40654321+yuvi55@users.noreply.github.com>
2023-12-16 02:42:50 -0500

    Update pusher.ts

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -5,13 +5,13 @@ export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_APP_SECRET!,
-  cluster: 'eu',
+  cluster: 'mt1',
   useTLS: true,
 })
 
 export const pusherClient = new PusherClient(
   process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   {
-    cluster: 'eu',
+    cluster: 'mt1',
   }
 )
